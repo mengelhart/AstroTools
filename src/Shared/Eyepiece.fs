98532a4b7aa81e094d40ddc59ed2d854cf34e5f5
Michael Engelhart <mike@pocketgalaxy.io>
2022-10-29 22:14:25 -0400

    Minor testing

--- src/Shared/Eyepiece.fs
+++ src/Shared/Eyepiece.fs
@@ -1,18 +1,20 @@
-namespace Equipment
+module Equipment
 
 open System
 
 [<Measure>] type mm
-[<Measure>] type inch
 [<Measure>] type degree
+[<Measure>] type kg
 
 type Manufacturer = Manufacturer of string
 type Design = Design of string
 type FocalLength = FocalLength of float<mm>
 type EyeRelief = EyeRelief of float<mm>
 type FieldStop = FieldStop of float<mm>
-type BarrelDiameter = BarrelDiameter of float<inch>
+type BarrelDiameter = BarrelDiameter of float<mm>
 type ApparentFieldOfView = ApparentFieldOfView of int<degree>
+type Aperture = Aperture of float<mm>
+type Weight = Weight of float<kg>
 
 type Eyepiece = {
     Id: Guid
@@ -24,16 +26,24 @@ type Eyepiece = {
     ApparentFieldOfView: ApparentFieldOfView
 }
 
-module Eyepiece =
-    let getEP  =
-        {
-            Id = Guid.NewGuid();
-            Manufacturer = Manufacturer "TeleVue";
-            Design = Design "Plossl";
-            FocalLength = FocalLength 9.0<mm>;
-            EyeRelief = EyeRelief 20.0<mm>;
-            BarrelDiameter = BarrelDiameter 2.0<inch>;
-            ApparentFieldOfView = ApparentFieldOfView 72<degree>
-        }
-
+type Telescope =  {
+    id: Guid
+    Manufacturer: Manufacturer
+    Design: Design
+    FocalLength: FocalLength
+    EyeRelief: EyeRelief
+    Aperture: Aperture
+    OTAWeight: Weight
+}
+//{
+//       Id = Guid.NewGuid();
+//       Manufacturer = Manufacturer "TeleVue";
+//       Design = Design "Plossl";
+//       FocalLength = FocalLength 9.0<mm>;
+//       EyeRelief = EyeRelief 20.0<mm>;
+//       BarrelDiameter = BarrelDiameter 2.0<inch>;
+//       ApparentFieldOfView = ApparentFieldOfView 72<degree>
+//   }
+let magnification (telescope: Telescope) (eyepiece: Eyepiece) : float =
+    telescope.FocalLength /  eyepiece.FocalLength
     
